90226df117b2a7202f407194eb8d43939ffbfe3a
Oscar Marcelo <oscarmarcelo@users.noreply.github.com>
2022-05-01 23:19:24 +0100

    Update `serverUser` to validate answer

--- generators/app/modules/deploy.js
+++ generators/app/modules/deploy.js
@@ -1,3 +1,10 @@
+import chalk from 'chalk';
+import figures from 'figures';
+
+import {error} from '../utils.js';
+
+
+
 /*
  * =============================================================================
  * Prompts
@@ -16,6 +23,30 @@ const prompts = generator =>
       name: 'serverUser',
       message: 'Server Username:',
       store: true,
+      validate(answer) {
+        if (answer.length === 0) {
+          return true;
+        }
+
+        const regex = /^[a-z_]([a-z\d_-]{0,31}|[a-z\d_-]{0,30}\$)$/;
+
+        const rules = [
+          'Must contain only letters, numbers, underscores, or hyphens;',
+          'Up to 32 characters;',
+          'First character must be lower case letter or underscore;',
+          'Last character may be a dollar sign ($).',
+        ]
+          .map(example => `${chalk.green(figures.tick)} ${chalk.cyan(example)}`);
+
+        return regex.test(answer)
+          ? true
+          : error([
+            'Must be a valid username.',
+            '',
+            chalk.dim('Rules:'),
+            ...rules,
+          ]);
+      },
       filter: answer => answer.trim(),
     },
     {
