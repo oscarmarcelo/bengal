55ce081fafbffd5ddee99a79be52c1f6756e74b8
Oscar Marcelo <oscarmarcelo@users.noreply.github.com>
2022-05-14 16:40:27 +0100

    Fix `index.php` calling a `main.css` on projects without `styles` task
    Also add a newline before the `main.js` reference for coherence with the Pug equivalent file.

--- templates/src/views/php/index.php
+++ templates/src/views/php/index.php
@@ -4,11 +4,14 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover">
   <title><%= project %></title>
+  <%_ if (styles) { -%>
   <link href="/assets/styles/main.css" rel="stylesheet">
+  <%_ } -%>
 </head>
 <body>
   <h1>Welcome to <%= project %>!</h1>
   <%_ if (scripts) { -%>
+
   <script src="/assets/scripts/main.js"></script>
   <%_ } -%>
 </body>
